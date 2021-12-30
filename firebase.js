b8b95fb44f106f73e1cfeca936a32bafaf098a46
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2021-12-30 22:17:18 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,7 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFFmdl8o4hqNidWZSKkQaCmg0l4yww6Fw",
-    authDomain: "whatsapp-2-56608.firebaseapp.com",
-    projectId: "whatsapp-2-56608",
-    storageBucket: "whatsapp-2-56608.appspot.com",
-    messagingSenderId: "796730635699",
-    appId: "1:796730635699:web:a320262133afe4fdda47c6",
-    measurementId: "G-326178QZ7K"
+  
   };
 
 
